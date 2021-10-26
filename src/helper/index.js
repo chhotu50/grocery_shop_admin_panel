89feffc3c6ae812856dcfb5cd168461f7d43f200
aditya <aditya@pixlerlab.com>
2021-10-26 10:32:53 +0530

    working on auth

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,24 +1,24 @@
 const REGEX = require("./Regex");
 
 const ROLE = {
-    ADMIN: 1,
-    CUSTOMER: 2,
+  ADMIN: 1,
+  CUSTOMER: 2,
 };
 const STATUS = {
-    ACTIVE: 1,
-    DEACTIVE: 2,
-    DELETED: 3,
+  ACTIVE: 1,
+  DEACTIVE: 2,
+  DELETED: 3,
 };
 
 const YESNO = {
-    YES: 1,
-    NO: 0,
+  YES: 1,
+  NO: 0,
 };
 
 export const helper = {
-    BASEURL: "http://localhost:4000/",
-    ROLE,
-    STATUS,
-    YESNO,
-    REGEX,
+  BASEURL: "http://192.168.1.4:4000/",
+  ROLE,
+  STATUS,
+  YESNO,
+  REGEX,
 };
--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react";
 import {
   CButton,
   CCard,
@@ -11,11 +11,48 @@ import {
   CInputGroup,
   CInputGroupPrepend,
   CInputGroupText,
-  CRow
-} from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+  CRow,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import FormValidation from "src/helper/FormValidation";
+import axios from "axios";
+const Register = (props) => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    password: "",
+    confirm_password: "",
+  });
 
-const Register = () => {
+  const [formError, setFormError] = useState({});
+  const handleInputChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+    setFormError({
+      ...formError,
+      [e.target.name]: FormValidation.registerForm(e.target.name, e.target.value),
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      formError.name === "" &&
+      formError.email === "" &&
+      formError.address === "" &&
+      formError.phone === "" &&
+      formError.password === ""
+    ) {
+      axios.post("register", data).then((res) => {
+        localStorage.setItem("user.token", res.data.token);
+        localStorage.setItem("user.role", res.data.data.role);
+        props.history.push("/");
+        console.log(res);
+      });
+    }
+  };
+
   return (
     <div className="c-app c-default-layout flex-row align-items-center">
       <CContainer>
@@ -23,7 +60,7 @@ const Register = () => {
           <CCol md="9" lg="7" xl="6">
             <CCard className="mx-4">
               <CCardBody className="p-4">
-                <CForm>
+                <CForm onSubmit={handleSubmit}>
                   <h1>Register</h1>
                   <p className="text-muted">Create your account</p>
                   <CInputGroup className="mb-3">
@@ -32,34 +69,99 @@ const Register = () => {
                         <CIcon name="cil-user" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="text" placeholder="Username" autoComplete="username" />
+                    <CInput
+                      type="text"
+                      placeholder="name"
+                      autoComplete="name"
+                      name="name"
+                      onChange={handleInputChange}
+                    />
                   </CInputGroup>
+                  {formError.name && <div>{formError.name}</div>}
                   <CInputGroup className="mb-3">
                     <CInputGroupPrepend>
                       <CInputGroupText>@</CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="text" placeholder="Email" autoComplete="email" />
+                    <CInput
+                      type="text"
+                      placeholder="Email"
+                      autoComplete="email"
+                      name="email"
+                      onChange={handleInputChange}
+                    />
                   </CInputGroup>
+                  {formError.email && <div>{formError.email}</div>}
                   <CInputGroup className="mb-3">
                     <CInputGroupPrepend>
                       <CInputGroupText>
-                        <CIcon name="cil-lock-locked" />
+                        <CIcon name="cilHome" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="password" placeholder="Password" autoComplete="new-password" />
+                    <CInput
+                      type="text"
+                      placeholder="Address"
+                      autoComplete="address"
+                      name="address"
+                      onChange={handleInputChange}
+                    />
                   </CInputGroup>
-                  <CInputGroup className="mb-4">
+                  {formError.address && <div>{formError.address}</div>}
+                  <CInputGroup className="mb-3">
                     <CInputGroupPrepend>
                       <CInputGroupText>
-                        <CIcon name="cil-lock-locked" />
+                        <CIcon name="cilPhone" />
                       </CInputGroupText>
                     </CInputGroupPrepend>
-                    <CInput type="password" placeholder="Repeat password" autoComplete="new-password" />
+                    <CInput
+                      type="text"
+                      placeholder="phone"
+                      autoComplete="phone"
+                      name="phone"
+                      onChange={handleInputChange}
+                    />
                   </CInputGroup>
-                  <CButton color="success" block>Create Account</CButton>
+                  {formError.phone && <div>{formError.phone}</div>}
+                  <CRow>
+                    <CCol>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupPrepend>
+                          <CInputGroupText>
+                            <CIcon name="cil-lock-locked" />
+                          </CInputGroupText>
+                        </CInputGroupPrepend>
+                        <CInput
+                          type="password"
+                          placeholder="Password"
+                          autoComplete="new-password"
+                          name="password"
+                          onChange={handleInputChange}
+                        />
+                      </CInputGroup>
+                      {formError.password && <div>{formError.password}</div>}
+                    </CCol>
+                    <CCol>
+                      <CInputGroup className="mb-4">
+                        <CInputGroupPrepend>
+                          <CInputGroupText>
+                            <CIcon name="cil-lock-locked" />
+                          </CInputGroupText>
+                        </CInputGroupPrepend>
+                        <CInput
+                          type="password"
+                          placeholder="Repeat password"
+                          autoComplete="new-password"
+                          name="new-password"
+                          onChange={handleInputChange}
+                        />
+                      </CInputGroup>
+                    </CCol>
+                  </CRow>
+                  <CButton color="success" block type="submit">
+                    Create Account
+                  </CButton>
                 </CForm>
               </CCardBody>
-              <CCardFooter className="p-4">
+              {/* <CCardFooter className="p-4">
                 <CRow>
                   <CCol xs="12" sm="6">
                     <CButton className="btn-facebook mb-1" block><span>facebook</span></CButton>
@@ -68,13 +170,13 @@ const Register = () => {
                     <CButton className="btn-twitter mb-1" block><span>twitter</span></CButton>
                   </CCol>
                 </CRow>
-              </CCardFooter>
+              </CCardFooter> */}
             </CCard>
           </CCol>
         </CRow>
       </CContainer>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
